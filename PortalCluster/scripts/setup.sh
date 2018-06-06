8930c071b3ba1ef49d86ca1d4156ccf6c9ce2f77
Ryan Bernardino <r.bernardino17@gmail.com>
2018-06-06 08:25:34 +0800

    Adds PortalCluster folder and setup.sh

---
+++ PortalCluster/scripts/setup.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+##################################################
+# Setup script common for all Linux boxes
+##################################################
+
+# Install packages
+yum install -y net-tools-2.0-0.17.20131004git.el7.x86_64
+
+# Set timezone (http://www.thegeekstuff.com/2010/09/change-timezone-in-linux/)
+rm -f /etc/localtime
+ln -s /usr/share/zoneinfo/Asia/Singapore /etc/localtime
+
+ulimit -n 10240
+
+echo 'Pausing for 10 secs'
+sleep 10
+
+echo 'Installing PowerShell for Linux'
+# Add MSFT's repo for RHEL-based packages
+# https://docs.microsoft.com/en-us/windows-server/administration/Linux-Package-Repository-for-Microsoft-Software
+sudo rpm -Uvh http://packages.microsoft.com/config/rhel/7/packages-microsoft-prod.rpm
+
+# Install PowerShell using yum
+yum install -y powershell
+
+sleep 5
+echo 'Installing Powershell DSC for Linux'
+wget https://github.com/Microsoft/omi/releases/download/v1.1.0-0/omi-1.1.0.ssl_100.x64.rpm \
+  -O /opt/omi.rpm
+wget https://github.com/Microsoft/PowerShell-DSC-for-Linux/releases/download/v1.1.1-294/dsc-1.1.1-294.ssl_100.x64.rpm \
+  -O /opt/dsc.rpm
+sudo rpm -Uvh /opt/omi.rpm /opt/dsc.rpm
+
+echo 'Pausing for 5 secs'
+sleep 5
+
+echo 'Configuring sshd'
+cp -p /vagrant/config/sshd_config /etc/ssh/sshd_config
+chmod 644 /etc/ssh/sshd_config
+service sshd restart
